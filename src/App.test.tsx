e141a505582fba85786c293461a961ff8d5d794a
Kishan Jobanputra <kishanj918@gmail.com>
2025-03-06 16:46:07 +0530

    refactor: test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { describe, it, expect } from "vitest";
 import add from "./add";
 
-describe("math functions", () => {
-  it("adds two numbers", () => {
+describe("String Calculator", () => {
+  it("return 0 if empty string", () => {
     expect(add("")).toBe(0);
   });
 });
