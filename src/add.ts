f92ba98feef6a843184516f90b6ed979a8ce7098
Kishan Jobanputra <kishanj918@gmail.com>
2025-03-06 16:45:04 +0530

    feat: Add inital test for empty string

---
+++ src/add.ts
@@ -0,0 +1,4 @@
+export default function add(numbers: string) {
+  if (numbers.length === 0) return 0;
+  return 0;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom", // for DOM APIs (needed for React)
+    globals: true, // optional, allows global `describe`, `it`, etc.
+    setupFiles: "./src/test/setup.js", // optional setup (more on this below)
+  },
+});
