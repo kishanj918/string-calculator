9266b44ad948710a714bac7c927755b21205f28c
Kishan Jobanputra <kishanj918@gmail.com>
2025-03-06 17:10:46 +0530

    feat: handle addition of multiple numbers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,4 +5,7 @@ describe("String Calculator", () => {
   it("return 0 if empty string", () => {
     expect(add("")).toBe(0);
   });
+  it("adds numbers", () => {
+    expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
+  });
 });
--- src/add.ts
+++ src/add.ts
@@ -1,4 +1,28 @@
-export default function add(numbers: string) {
+export default function add(numbers: string): number {
   if (numbers.length === 0) return 0;
-  return 0;
+
+  const delimiters = new Set([","]);
+  let counter = 0;
+  let total = 0;
+  let currentNumber = "";
+
+  while (counter < numbers.length) {
+    const char = numbers[counter];
+
+    if (delimiters.has(char)) {
+      if (currentNumber !== "") {
+        total += Number(currentNumber);
+        currentNumber = "";
+      }
+    } else {
+      currentNumber += char;
+    }
+
+    counter++;
+  }
+  if (currentNumber !== "") {
+    total += Number(currentNumber);
+  }
+
+  return total;
 }
