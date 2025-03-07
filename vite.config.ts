ce6b99f7dcf5e4327473a8676553d2a6a5b28bfe
Kishan Jobanputra <kishanj918@gmail.com>
2025-03-07 09:17:57 +0530

    chore: deploy app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/string-calculator/",
   test: {
     environment: "jsdom", // for DOM APIs (needed for React)
     globals: true, // optional, allows global `describe`, `it`, etc.
